c8cf95ca976b2f160b4fc89844c34b4e8709b581
鲍佳玮 <bjw_0627@163.com>
2020-09-21 15:51:11 +0800

    完成基本的按钮ui组件(主要按钮、警告按钮、危险按钮、成功按钮、信息按钮)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <pos-button></pos-button>
+    <pos-button>默认按钮</pos-button>
+    <pos-button type="primary">主要按钮</pos-button>
+    <pos-button type="warning">警告按钮</pos-button>
+    <pos-button type="danger">危险按钮</pos-button>
+    <pos-button type="success">成功按钮</pos-button>
+    <pos-button type="info">信息按钮</pos-button>
     <pos-icon></pos-icon>
   </div>
 </template>
--- src/packages/button.vue
+++ src/packages/button.vue
@@ -1,11 +1,125 @@
 <template>
-  <div>
-    button
-  </div>
+  <button class="pos-button" :class="btnClass">
+    <span v-if="this.$slots.default">
+      <slot></slot>
+    </span>
+  </button>
 </template>
 
 <script>
 export default {
   name: 'pos-button',
+  props: {
+    type: {
+      type: String,
+      default: '',
+      validator(type) {
+        if (
+          type &&
+          !['warning', 'success', 'danger', 'primary', 'info'].includes(type)
+        ) {
+          console.error(
+            'type类型必须为: ' +
+              ['warning', 'success', 'danger', 'primary', 'info'].join('、'),
+          )
+        }
+        return true
+      },
+    },
+  },
+  computed: {
+    btnClass() {
+      let classes = []
+      if (this.type) {
+        classes.push(`pos-button-${this.type}`)
+      }
+      return classes
+    },
+  },
 }
 </script>
+
+<style lang="scss">
+@import '../styles/_var.scss';
+$height: 42px;
+$font-size: 16px;
+$color: #606266;
+$border-color: #dcdfe6;
+$background: #ecf5ff;
+$active-color: #3a8ee6;
+.pos-button {
+  border-radius: $border-radius;
+  border: 1px solid $border-color;
+  height: $height;
+  line-height: 1;
+  font-size: $font-size;
+  cursor: pointer;
+  padding: 12px 20px;
+  display: inline-flex;
+  justify-content: center;
+  vertical-align: middle;
+  user-select: none;
+  &:hover {
+    border-color: $border-color;
+    background-color: $background;
+  }
+  &:focus,
+  &:active {
+    color: $active-color;
+    border-color: 1px solid $active-color;
+    background-color: $background;
+    outline: none;
+  }
+  @each $type,
+    $color
+      in (
+        primary: $primary,
+        success: $success,
+        info: $info,
+        warning: $warning,
+        danger: $danger
+      )
+  {
+    &-#{$type} {
+      background: #{$color};
+      border: 1px solid #{$color};
+      color: #ffffff;
+    }
+  }
+
+  @each $type,
+    $color
+      in (
+        primary: $primary-hover,
+        success: $success-hover,
+        info: $info-hover,
+        warning: $warning-hover,
+        danger: $danger-hover
+      )
+  {
+    &-#{$type}:hover {
+      background: #{$color};
+      border: 1px solid #{$color};
+      color: #ffffff;
+    }
+  }
+
+  @each $type,
+    $color
+      in (
+        primary: $primary-active,
+        success: $success-active,
+        info: $info-active,
+        warning: $warning-active,
+        danger: $danger-active
+      )
+  {
+    &-#{$type}:active,
+    &-#{$type}:focus {
+      background: #{$color};
+      border: 1px solid #{$color};
+      color: #ffffff;
+    }
+  }
+}
+</style>
