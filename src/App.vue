8e737c9d12eac15a0f37961aa140cb80fb6a0be5
鲍佳玮 <bjw_0627@163.com>
2020-09-24 23:26:54 +0800

    增加input组件，v-model双向绑定

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <pos-input></pos-input>
+    <pos-input v-model="value" type="text" placeholder="请输入内容" name="username"></pos-input>
+    <pos-input v-model="value" type="password" placeholder="请输入内容" name="username"></pos-input>
+    {{ value }}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      value: ''
+    }
+  }
 }
 </script>
 
--- src/packages/Form/input/input.vue
+++ src/packages/Form/input/input.vue
@@ -1,11 +1,55 @@
 <template>
-  <div>
-    <input type="text" name="" id="" />
+  <div class="pos-input">
+    <input
+      :type="type"
+      :name="name"
+      :placeholder="placeholder"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'pos-input'
+  name: 'pos-input',
+  props: {
+    name: {
+      type: String,
+      default: null
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    placeholder: {
+      type: String,
+      default: '请输入内容'
+    },
+    value: {
+      type: String,
+      default: ''
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+@import '@/styles/_var.scss';
+.pos-input {
+  display: inline-flex;
+  input {
+    padding: 8px;
+    width: 150px;
+    height: 42px;
+    border-radius: $border-radius;
+    border: 1px solid #dcdfe6;
+
+    &:focus {
+      border: 1px solid $primary;
+      outline: none;
+      box-shadow: inset -1px 0px 2px $primary, inset 1px 1px 1px $primary;
+    }
+  }
+}
+</style>
