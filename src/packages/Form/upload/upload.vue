d151b9ac33a43f822f68f8509292d5a572b8cac1
鲍佳玮 <bjw_0627@163.com>
2020-09-26 22:48:53 +0800

    upload组件: 上传文件控制fileList列表，将外部传入的fileList和用户新上传的file，结合存入组件内部

--- src/packages/Form/upload/upload.vue
+++ src/packages/Form/upload/upload.vue
@@ -10,6 +10,7 @@
       @change="handleChange"
       :name="name"
       ref="input"
+      class="hidden"
     />
     <div>
       <slot name="tip"></slot>
@@ -20,6 +21,12 @@
 <script>
 export default {
   name: 'pos-upload',
+  data() {
+    return {
+      tempIndex: 1,
+      files: []
+    }
+  },
   props: {
     name: {
       type: String,
@@ -42,6 +49,19 @@ export default {
     onError: Function,
     onProgress: Function
   },
+  watch: {
+    // 默认将外部传入的fileList添加到组件内的变量files里
+    fileList: {
+      immediate: true,
+      handler(fileList) {
+        this.files = fileList.map(item => {
+          item.uid = Date.now() + this.tempIndex++
+          item.status = 'success'
+          return item
+        })
+      }
+    }
+  },
   methods: {
     handleClick() {
       this.$refs.input.value = ''
@@ -55,7 +75,27 @@ export default {
       if (this.limit && this.fileList.length + files.length > this.limit) {
         return this.onExceed && this.onExceed(files, this.fileList)
       }
-    }
+      const filesArr = [...files]
+      filesArr.forEach(rawFile => {
+        this.handleStart(rawFile)
+        this.upload(rawFile)
+      })
+    },
+    handleStart(rawFile) {
+      rawFile.uid = Math.random() + this.tempIndex++
+      let file = {
+        status: 'ready',
+        name: rawFile.name,
+        size: rawFile.size,
+        percentage: 0,
+        uid: rawFile.uid,
+        raw: rawFile
+      }
+      // 将当前用户上传的文件push到files里
+      // files里将会有已上传的和未上传的文件
+      this.files.push(file)
+    },
+    upload() {}
   }
 }
 </script>
@@ -65,6 +105,9 @@ export default {
 .pos-upload {
   .pos-upload-btn {
     display: inline-block;
+  }
+  .hidden {
+    display: none;
   }
 }
 </style>
