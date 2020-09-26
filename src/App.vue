2c79892119562ad935e01e7facecfb9acb7bd080
鲍佳玮 <bjw_0627@163.com>
2020-09-26 22:21:25 +0800

    upload组件: 上传文件超过限制，调用onExceed

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,17 @@
       action="http://localhost:3000/upload"
       :file-list="fileList"
       :limit="3"
+      accept="image/jpeg"
       :multiple="true"
       :on-exceed="handleExceed"
       :on-change="handleChange"
       :on-success="handleSuccess"
       :on-error="handleError"
       :on-progress="handleProgress"
-    ></pos-upload>
+    >
+      <pos-button icon="shangchuan" type="primary">点击上传</pos-button>
+      <div slot="tip">只能上传jpg/png文件，且不能超过500kb</div>
+    </pos-upload>
   </div>
 </template>
 
@@ -27,7 +31,10 @@ export default {
     }
   },
   methods: {
-    handleExceed() {},
+    handleExceed(files, fileList) {
+      console.log(files, fileList)
+      console.log('用户传递的已经超过预期数量了')
+    },
     handleChange() {},
     handleSuccess() {},
     handleError() {},
--- src/packages/Form/upload/upload.vue
+++ src/packages/Form/upload/upload.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="pos-upload">upload</div>
+  <div class="pos-upload">
+    <div @click="handleClick" class="pos-upload-btn">
+      <slot></slot>
+    </div>
+    <input
+      type="file"
+      :accept="accept"
+      :multiple="multiple"
+      @change="handleChange"
+      :name="name"
+      ref="input"
+    />
+    <div>
+      <slot name="tip"></slot>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -18,6 +33,7 @@ export default {
       type: Array,
       default: () => []
     },
+    accept: String,
     limit: Number,
     multiple: Boolean,
     onExceed: Function,
@@ -26,10 +42,29 @@ export default {
     onError: Function,
     onProgress: Function
   },
-  methods: {}
+  methods: {
+    handleClick() {
+      this.$refs.input.value = ''
+      this.$refs.input.click()
+    },
+    handleChange(e) {
+      const files = e.target.files
+      this.uploadFiles(files)
+    },
+    uploadFiles(files) {
+      if (this.limit && this.fileList.length + files.length > this.limit) {
+        return this.onExceed && this.onExceed(files, this.fileList)
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 @import '@/styles/_var.scss';
+.pos-upload {
+  .pos-upload-btn {
+    display: inline-block;
+  }
+}
 </style>
